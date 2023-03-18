4ae1e8b3ee523988e3689e7cb61c52ac0d4568d6
Ricky Hu <ricky9667@gmail.com>
2023-03-18 11:21:55 +0800

    feat: open in app webview

--- app/src/main/java/com/rickyhu/hdriver/SearchDialogFragment.kt
+++ app/src/main/java/com/rickyhu/hdriver/SearchDialogFragment.kt
@@ -2,6 +2,8 @@ package com.rickyhu.hdriver
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -10,6 +12,7 @@ import com.rickyhu.hdriver.databinding.DialogSearchBinding
 
 class SearchDialogFragment : DialogFragment() {
     private lateinit var binding: DialogSearchBinding
+    private val NHENTAI_BASE_URL = "https://nhentai.net/g"
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         binding = DialogSearchBinding.inflate(LayoutInflater.from(activity))
@@ -19,9 +22,24 @@ class SearchDialogFragment : DialogFragment() {
             .setTitle(getString(R.string.text_set_car_information))
 
         binding.buttonSearch.setOnClickListener {
-            Log.d("SearchDialogFragment", "Search button clicked")
+            val godNumberText = binding.edittextGodNumber.text.toString()
+            val godNumber = godNumberText.toIntOrNull()
+
+            if (godNumber != null) {
+                val url = "$NHENTAI_BASE_URL/$godNumber"
+                val intent = Intent(activity, WebViewActivity::class.java)
+                intent.putExtra("url", url)
+                startActivity(intent)
+            } else {
+                Log.e("SearchDialogFragment", "unable to load view")
+            }
         }
 
         return builder.create()
+    }
+
+    private fun openWebView(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/rickyhu/hdriver/WebViewActivity.kt
@@ -0,0 +1,30 @@
+package com.rickyhu.hdriver
+
+import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class WebViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view)
+
+        val webView = findViewById<WebView>(R.id.web_view)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                view?.loadUrl(request?.url.toString())
+                return true
+            }
+        }
+
+        val url = intent.getStringExtra("url")
+        if (url != null)
+            webView.loadUrl(url)
+    }
+}
