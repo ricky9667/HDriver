83ae840509a13da18394a207fd5e04a7f0c04130
Ricky Hu <ricky9667@gmail.com>
2023-04-03 20:21:23 +0800

    feat: add confirm dialog before delete item

--- app/src/main/java/com/rickyhu/hdriver/presentation/RecentListFragment.kt
+++ app/src/main/java/com/rickyhu/hdriver/presentation/RecentListFragment.kt
@@ -1,5 +1,6 @@
 package com.rickyhu.hdriver.presentation
 
+import android.app.AlertDialog
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
@@ -11,6 +12,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.rickyhu.hdriver.R
 import com.rickyhu.hdriver.data.model.GodItem
 import com.rickyhu.hdriver.databinding.FragmentRecentItemListBinding
 import com.rickyhu.hdriver.viewmodel.RecentListViewModel
@@ -42,7 +44,7 @@ class RecentListFragment : Fragment() {
                 override fun onClick(item: GodItem) = copyUrlToClipboard(item.url)
             })
             setOnDeleteClickListener(object : RecentListAdapter.RecentItemClickListener {
-                override fun onClick(item: GodItem) = viewModel.deleteRecentItem(item)
+                override fun onClick(item: GodItem) = deleteItem(item)
             })
         }
 
@@ -62,14 +64,18 @@ class RecentListFragment : Fragment() {
     }
 
     private fun copyUrlToClipboard(url: String) {
-        val clipboard = requireActivity()
-            .getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clipboard =
+            requireActivity().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
         val clip = ClipData.newPlainText("url", url)
         clipboard.setPrimaryClip(clip)
     }
 
-    companion object {
-        // TODO: extract url
-        const val baseUrl = "https://nhentai.net/g/{query}"
+    private fun deleteItem(item: GodItem) {
+        val dialog = AlertDialog.Builder(requireContext()).setTitle("刪除車號")
+            .setMessage("你確定要刪除 ${item.number} 嗎？")
+            .setPositiveButton(getString(R.string.dialog_ok)) { _, _ ->
+                viewModel.deleteRecentItem(item)
+            }.setNegativeButton(getString(R.string.dialog_cancel)) { _, _ -> }.create()
+        dialog.show()
     }
 }
